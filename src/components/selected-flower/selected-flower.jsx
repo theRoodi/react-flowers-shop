94dd64ee62527632b2df7a2191756e3af6841545
SergSvet87 <svetlichnij1905@gmail.com>
2022-12-15 09:13:07 +0200

    added selected-flower

--- src/components/selected-flower/selected-flower.jsx
+++ src/components/selected-flower/selected-flower.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from "react";
 
 export const SelectedFlower = () => {
   return (
-    <div>shop-bottom</div>
-  )
-}
+    <section className="selected-flower">
+      <div className="container">
+        <div className="selected-flower__wrapper">
+          <div className="selected-flower__img">
+            <img
+              src=""
+              alt=""
+            />
+          </div>
+          <div className="selected-flower__description">
+            <h6 className="title-h6">Sun flower</h6>
+            <p className="selected-flower__text">
+              Make every day brighter with our abundant bouquet of fresh
+              sunflowers. These radiant, long-lasting blooms bring that
+              just-picked-from-the-meadow feeling to birthdays, get well wishes,
+              or any day you want to make someone you care about smile.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
