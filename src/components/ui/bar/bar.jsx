6a56165eb59a91ed1b11cdf818eb454b75b1268f
Ten-Do <rydenko.urii@mail.ru>
2022-12-18 14:32:53 +0300

    изменил карточку товара

--- src/components/ui/bar/bar.jsx
+++ src/components/ui/bar/bar.jsx
@@ -1,22 +1,59 @@
 import React from "react"
 import "./bar.scss"
 
-export const Bar = () => {
-    return (
-    <div className="bar">
-      <div className="bar__filter">
-        Filtering
-      </div>
-      <div className="bar__search">
-        <form action="">
-            <input type="search" placeholder="Search.." />
-            
-        </form>
-      </div>
-      <div className="bar__sort">
-        Sorting
-      </div>
-    </div>
-    )
+let openFilterNav = () => {
+  document.querySelector(".filterNav").style.display = "block";
 }
-      
+
+let closeFilterNav = () => {
+  document.querySelector(".filterNav").style.display = "none";
+}
+
+
+
+
+
+
+const FilterNav = () => {
+  return (
+    <>
+      <div class="filterNav">
+        <div class="filterNav-content">
+          <div class="modal-header">
+            <span class="close" onClick={closeFilterNav}>x</span>
+          </div>
+          <div class="modal-body">
+            <p>Some text in the Modal Body</p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export const Bar = () => {
+  return (
+    <>
+      <FilterNav />
+      <div className="bar">
+        <button className="bar__filter" onClick={openFilterNav}>
+          Filtering
+        </button>
+        <div className="bar__search">
+          <form action="">
+            <input type="search" placeholder="Search.." />
+
+          </form>
+        </div>
+        <div className="bar__sort">
+          <label>Sorting by</label>
+          <select name="Sorting" id="sort_options">
+            <option value="По популярности">По популярности</option>
+            <option value="По возрастанию цены">По возрастанию цены</option>
+            <option value="По убыванию цены">По убыванию цены</option>
+          </select>
+        </div>
+      </div>
+    </>
+  )
+}
--- src/components/ui/goods/goodsCard.jsx
+++ src/components/ui/goods/goodsCard.jsx
@@ -7,16 +7,15 @@ import cartShop from "../../../assets/svg/cart.svg";
 export const GoodsCard = () => {
   return (
     <div className="flexContainer">
-
-    <div className="goodsCard">
-      <img className="goodsCard__img" src={flower} alt="flower is lost.." />
-      <h6 className="goodsCard__name">Flower's Name</h6>
-      <span className="goodsCard__price">0$</span>
-      <a href="/" className="goodsCard__addToCard"><img
-                src={cartShop}
-                alt="cart"
-              />Add to cart</a>
-    </div>
+      <div className="goodsCard">
+        <img className="goodsCard__img" src={flower} alt="flower is lost.." />
+        <h6 className="goodsCard__name">Flower's Name</h6>
+        <span className="goodsCard__price">0$</span>
+        <button href="/" className="goodsCard__addToCard"><img
+          src={cartShop}
+          alt="cart"
+        />Add to cart</button>
+      </div>
     </div>
   );
 };
