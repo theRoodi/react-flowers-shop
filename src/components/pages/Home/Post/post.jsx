faf8fa76fce9809d7696bf9ab3ef4ea3c84ee4c8
AnyaRatusnyak <anyaaboyko@gmail.com>
2022-12-20 03:18:58 +0200

    changed the post

--- src/components/pages/Home/Post/post.jsx
+++ src/components/pages/Home/Post/post.jsx
@@ -2,30 +2,36 @@ import React from "react";
 import "./post.scss";
 import postImg from "../../../../assets/img/post-img/postImg.png";
 import ava from "../../../../assets/img/ava/Ava.png";
+import eye from "../../../../assets/svg/eye.svg";
+import hurt from "../../../../assets/svg/hurt.svg";
 
 export const Post = () => {
   return (
-    <div className="container">
-      <header>
-        <div className="row">
-          <div className="col-6">
-            <img src={ava} alt="" /> name
-          </div>
-          <div className="col-6">date</div>
+    <div className="container post">
+      <div className="row post_header">
+        <div className="col-6 ">
+          <img src={ava} alt="avatar" /> <b>Emily</b>
         </div>
-      </header>
-      <div>
-        <img src={postImg} alt="" />
+        <div className="col-6 post_header-data">2022/22/8</div>
       </div>
-      <div>
-        <h7>Best flowers for inside home</h7>
+      <div className="post_image">
+        <img src={postImg} alt="design" />
+      </div>
+      <div className="post_description">
+        <h6>Best flowers for inside home</h6>
         <p>
-          All the flowers are best for your lovly house just get the one you
-          love the most 😊
+          All the flowers are best for your lovly house
+          <br /> just get the one you love the most 😊
         </p>
-        <div className="row">
-          <div className="col-6">icon name</div>
-          <div className="col-6">Read more</div>
+        <div className="row post_footer">
+          <div className="col-6 ps-4">
+            <img src={hurt} alt="hurt" />
+            15
+            <img src={eye} alt="eye" /> 2001
+          </div>
+          <div className="col-6">
+            <button>Read more</button>
+          </div>
         </div>
       </div>
     </div>
